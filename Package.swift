dd974838328e9f7106d8e4cea7e53afe723f9aac
yantl0 <tongliang@galxe.com>
2026-04-07 21:30:35 +0800

    feat: add Package.swift for SPM binary distribution

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "VeryMobileSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "VeryMobileSDK",
+            targets: ["VeryMobileSDK", "PalmAPISaas"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "VeryMobileSDK",
+            path: "VeryMobileSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "PalmAPISaas",
+            path: "PalmAPISaas.xcframework"
+        ),
+    ]
+)
